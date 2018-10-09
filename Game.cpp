c87bfbef51135d895788cf6c640b4fc7b1af6d3d
Igor <80776287+i9or@users.noreply.github.com>
2018-10-09 22:51:11 +0200

    Implement pong

--- Game.cpp
+++ Game.cpp
@@ -6,7 +6,9 @@
 
 Game::Game() :
         mWindow(nullptr),
-        mIsRunning(true) {
+        mIsRunning(true),
+        mTicksCount(0),
+        mPaddleDirection(0) {
 
 }
 
@@ -45,6 +47,13 @@ bool Game::Initialize() {
     SDL_SetWindowSize(mWindow, mWidth, mHeight);
     // -------------------------------------------
 
+    mPaddlePosition.x = mThickness + mThickness / 2.0f;
+    mPaddlePosition.y = mHeight / 2.0f;
+    mBallPosition.x = mWidth / 2.0f;
+    mBallPosition.y = mHeight / 2.0f;
+    mBallVelocity.x = -200.0f;
+    mBallVelocity.y = 235.0f;
+
     return true;
 }
 
@@ -80,15 +89,106 @@ void Game::ProcessInput() {
     if (state[SDL_SCANCODE_ESCAPE]) {
         mIsRunning = false;
     }
+
+    mPaddleDirection = 0;
+
+    if (state[SDL_SCANCODE_W]) {
+        mPaddleDirection -= 1;
+    }
+
+    if (state[SDL_SCANCODE_S]) {
+        mPaddleDirection += 1;
+    }
 }
 
 void Game::UpdateGame() {
+    while (!SDL_TICKS_PASSED(SDL_GetTicks(), mTicksCount + 16));
 
+    float deltaTime = (SDL_GetTicks() - mTicksCount) / 1000.0f;
+
+    if (deltaTime > 0.05f) {
+        deltaTime = 0.05f;
+    }
+
+    mTicksCount = SDL_GetTicks();
+
+    if (mPaddleDirection != 0) {
+        mPaddlePosition.y += mPaddleDirection * 300.0f * deltaTime;
+
+        if (mPaddlePosition.y < (mPaddleHeight / 2.0f + mThickness)) {
+            mPaddlePosition.y = mPaddleHeight / 2.0f + mThickness;
+        }
+
+        if (mPaddlePosition.y > (mHeight - mPaddleHeight / 2.0f - mThickness)) {
+            mPaddlePosition.y = mHeight - mPaddleHeight / 2.0f - mThickness;
+        }
+    }
+
+    mBallPosition.x += mBallVelocity.x * deltaTime;
+    mBallPosition.y += mBallVelocity.y * deltaTime;
+
+    float diff = mPaddlePosition.y - mBallPosition.y;
+    diff = (diff > 0.0f) ? diff : -diff;
+
+    if (diff <= mPaddleHeight / 2.0f &&
+        mBallPosition.x <= (mPaddlePosition.x + mThickness / 2.0f) &&
+        mBallPosition.x >= (mPaddlePosition.x - mThickness / 2.0f) &&
+        mBallVelocity.x < 0.0f) {
+        mBallVelocity.x *= -1.0;
+    } else if (mBallPosition.x <= 0.0f) {
+        mIsRunning = false;
+    }
+
+    if (mBallPosition.x >= (mWidth - mThickness) && mBallVelocity.x > 0.0f) {
+        mBallVelocity.x *= -1.0f;
+    }
+
+    if (mBallPosition.y <= mThickness && mBallVelocity.y < 0.0f) {
+        mBallVelocity.y *= -1;
+    }
+
+    if (mBallPosition.y >= (mHeight - mThickness) &&
+        mBallVelocity.y > 0.0f) {
+        mBallVelocity.y *= -1;
+    }
 }
 
 void Game::GenerateOutput() {
     SDL_SetRenderDrawColor(mRenderer, 41, 128, 185, 255);
     SDL_RenderClear(mRenderer);
+
+    // Drawing walls
+    SDL_SetRenderDrawColor(mRenderer, 255, 255, 255, 255);
+    SDL_Rect wall{0, 0, mWidth, mThickness};
+    SDL_RenderFillRect(mRenderer, &wall);
+
+    wall.y = mHeight - mThickness;
+    SDL_RenderFillRect(mRenderer, &wall);
+
+    wall.x = mWidth - mThickness;
+    wall.y = 0;
+    wall.w = mThickness;
+    wall.h = mWidth;
+    SDL_RenderFillRect(mRenderer, &wall);
+
+    // Drawing ball
+    SDL_Rect ball{
+            static_cast<int>(mBallPosition.x - mThickness / 2.f),
+            static_cast<int>(mBallPosition.y - mThickness / 2.f),
+            mThickness,
+            mThickness
+    };
+    SDL_RenderFillRect(mRenderer, &ball);
+
+    // Drawing paddle
+    SDL_Rect paddle{
+            static_cast<int>(mPaddlePosition.x - mThickness / 2.f),
+            static_cast<int>(mPaddlePosition.y - mPaddleHeight / 2.f),
+            mThickness,
+            mPaddleHeight
+    };
+    SDL_RenderFillRect(mRenderer, &paddle);
+
     SDL_RenderPresent(mRenderer);
 }
 
--- Game.h
+++ Game.h
@@ -7,6 +7,11 @@
 
 #include <SDL.h>
 
+struct Vector2 {
+    float x;
+    float y;
+};
+
 class Game {
 public:
     Game();
@@ -26,11 +31,18 @@ private:
 
     SDL_Window *mWindow;
     SDL_Renderer *mRenderer;
-
+    Uint32 mTicksCount;
     bool mIsRunning;
 
-    const int mWidth = 1024;
-    const int mHeight = 768;
+    static const int mWidth = 1024;
+    static const int mHeight = 768;
+    static const int mThickness = 15;
+    static constexpr int mPaddleHeight = mThickness * 6;
+
+    int mPaddleDirection;
+    Vector2 mPaddlePosition;
+    Vector2 mBallPosition;
+    Vector2 mBallVelocity;
 };
 
 
